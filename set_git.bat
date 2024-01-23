41d280048402efea4bfae7bfde12ad8cd0c7c4f0
rawan sadeq <rawan.sadeq8@mail.com>
2024-01-23 13:43:42 +0200

    first commit

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/RawanSadeq5/web.git
+git remote add origin https://github.com/RawanSadeq5/WebCalculator.git
 git push -u origin main
